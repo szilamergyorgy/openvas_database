11568b9d43a2fd03d564b8ad03571b51926e1f79
szilamergyorgy <52220446+szilamergyorgy@users.noreply.github.com>
2022-12-22 06:31:26 -0500

    Add files via upload

---
+++ openvas_database.sh
@@ -0,0 +1,8 @@
+# Change to the DOWNLOAD_DIR as root
+cd $DOWNLOAD_DIR
+
+# Change to the greenbone-community-container folder
+cd greenbone-community-container
+
+# Run docker-compose pull
+docker-compose pull
